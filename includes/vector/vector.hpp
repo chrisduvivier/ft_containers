d1ff517d2c54db93c2dce19f416c0a709396c7a1
Chris Duvivier <cduvivie@student.s19.be>
2021-10-25 16:36:14 +0200

    deleted test binary from repo. modified resize to allocate exactly the sie given as param

--- includes/vector/vector.hpp
+++ includes/vector/vector.hpp
@@ -291,10 +291,7 @@ namespace ft
 			}
 			else // create an _array with twice the previous capacity
 			{
-				size_type new_capacity = (_capacity > 0) ? _capacity : 1;
-				while (new_capacity < n) 		// double up until it fits 'n' -> this allows a margin for more potential elements to be added.
-					new_capacity *= 2;
-				this->reserve(new_capacity);	// Reserve a new array with new_capacity, and copy previous content to it.
+				this->reserve(n);	// Reserve a new array with new_capacity, and copy previous content to it.
 				for (size_type i = _size; i < n; i++)
 					_alloc.construct(&_array[i], val);
 				this->_size = n;
