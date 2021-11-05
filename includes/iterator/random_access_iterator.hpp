20978660e3fef3e643d76b513c4c9f37b98d6f0d
Kovlin <rlinkov@fu-r2-p8.s19.be>
2021-11-05 15:36:11 +0100

    traits

--- includes/iterator/random_access_iterator.hpp
+++ includes/iterator/random_access_iterator.hpp
@@ -23,17 +23,17 @@ namespace ft
 		public:
 			
 			typedef 		random_access_iterator			It;
-			typedef	typename ft::iterator<random_access_iterator_tag, T>::iterator_category	iterator_category;
-			typedef	typename ft::iterator<random_access_iterator_tag, T>::pointer			pointer;
-			typedef	typename ft::iterator<random_access_iterator_tag, T>::reference			reference;
-			typedef	typename ft::iterator<random_access_iterator_tag, T>::value_type		value_type;
-			typedef	typename ft::iterator<random_access_iterator_tag, T>::difference_type	difference_type;
+			// typedef	typename ft::iterator<random_access_iterator_tag, T>::iterator_category	iterator_category;
+			// typedef	typename ft::iterator<random_access_iterator_tag, T>::pointer			pointer;
+			// typedef	typename ft::iterator<random_access_iterator_tag, T>::reference			reference;
+			// typedef	typename ft::iterator<random_access_iterator_tag, T>::value_type		value_type;
+			// typedef	typename ft::iterator<random_access_iterator_tag, T>::difference_type	difference_type;
 
-			//typedef	typename ft::iterator_traits<It>::iterator_category	iterator_category;
-			//typedef	typename ft::iterator_traits<It>::pointer			pointer;
-			//typedef	typename ft::iterator_traits<It>::reference			reference;
-			//typedef	typename ft::iterator_traits<It>::value_type		value_type;
-			//typedef	typename ft::iterator_traits<It>::difference_type	difference_type;
+			typedef	typename ft::iterator_traits<It>::iterator_category	iterator_category;
+			typedef	typename ft::iterator_traits<It>::pointer			pointer;
+			typedef	typename ft::iterator_traits<It>::reference			reference;
+			typedef	typename ft::iterator_traits<It>::value_type		value_type;
+			typedef	typename ft::iterator_traits<It>::difference_type	difference_type;
 			
 			/* iterator Constructor: default, parameter, copy, assign */
 			random_access_iterator() : _ptr(nullptr) {}
